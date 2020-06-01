54307ad460e1c17238da4eface688594844b6038
Alexey Korablin <alex8456@bk.ru>
2020-06-02 00:05:06 +0300

    #18 Using Axios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
   componentDidMount() {
     axios
       .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => console.log(response));
+      .then((response) => {
+        console.log(response);
+        this.setState({ posts: response.data });
+      });
   }
 
   render() {
+    const posts = this.state.posts.slice(0, 10);
     return (
       <div className='container'>
         <h4 className='center'>Home</h4>
@@ -18,6 +29,18 @@ export class Home extends Component {
           eveniet similique distinctio et voluptate animi! Quod
           excepturi sed hic, rerum molestias nam nulla cumque delectus.
         </p>
+        {posts.length ? (
+          posts.map((post) => (
+            <div key={post.id} className='post card'>
+              <div className='card-content'>
+                <span className='card-title'>{post.title}</span>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className='center'>No posts yet</div>
+        )}
       </div>
     );
   }
